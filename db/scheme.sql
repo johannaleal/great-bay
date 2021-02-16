50286fa078eb270191257346ec3d7af1d84b8ead
banuakman <banu@asterya.com>
2021-02-16 11:27:31 -0500

    create items table

--- db/scheme.sql
+++ db/scheme.sql
@@ -1,3 +1,9 @@
 DROP DATABASE IF EXISTS greatbay_db;
 
 CREATE DATABASE greatbay_db;
+
+CREATE TABLE items(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    item VARCHAR(30) NOT NULL,
+    category VARCHAR(30) NOT NULL,
+)
